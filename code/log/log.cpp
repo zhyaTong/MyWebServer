967a89e28b5a13dacc8c1b12dbd38934b5a26827
yya1230 <3300400191@qq.com>
2026-01-09 10:58:06 +0800

    添加日志

--- code/log/log.cpp
+++ code/log/log.cpp
@@ -16,10 +16,10 @@ Log::~Log()
     {
         while (!deque_->empty())
         {
-            deque_->flush();
+            deque_->flush();    
         }
-        deque_->Close();
-        writeThread_->join();
+        deque_->Close();    //pop()返回false,线程退出循环
+        writeThread_->join();   //当前线程（主线程）阻塞等待日志写线程执行完毕
     }
     if (fp_)
     {
@@ -64,8 +64,8 @@ void Log::init(int level = 1, const char *path, const char *suffix, int maxDeque
 
     lineCount_ = 0;
 
-    time_t timer = time(nullptr);
-    struct tm *sysTime = localtime(&timer);
+    time_t timer = time(nullptr);   //获得当前时间戳
+    struct tm *sysTime = localtime(&timer); //转换为本地时间结构
     struct tm t = *sysTime;
     path_ = path;
     suffix_ = suffix;
@@ -82,10 +82,10 @@ void Log::init(int level = 1, const char *path, const char *suffix, int maxDeque
             flush();
             fclose(fp_);
         }
-        fp_ = fopen(fileName, "a");
+        fp_ = fopen(fileName, "a"); //追加
         if (fp_ == nullptr)
         {
-            mkdir(path_, 0777);
+            mkdir(path_, 0777); //所有用户都可读/写/执行
             fp_ = fopen(fileName, "a");
         }
         assert(fp_ != nullptr);
@@ -94,13 +94,14 @@ void Log::init(int level = 1, const char *path, const char *suffix, int maxDeque
 
 void Log::write(int level, const char *format, ...)
 {
-    struct timeval now = {0, 0};
-    gettimeofday(&now, nullptr);
+    struct timeval now = {0, 0};    //{秒，微秒}
+    gettimeofday(&now, nullptr);    //获取当前时间
     time_t tSec = now.tv_sec;
-    struct tm *sysTime = localtime(&tSec);
+    struct tm *sysTime = localtime(&tSec);  //localtime() 只能处理 time_t（秒级时间戳）
     struct tm t = *sysTime;
     va_list vaList;
 
+    //如果日志日期不是当天或者日志超过最大行数，创建一个新的日志文件
     if (toDay_ != t.tm_mday || (lineCount_ && (lineCount_ % MAX_LINES == 0)))
     {
         std::unique_lock<std::mutex> locker(mtx_);
@@ -139,20 +140,21 @@ void Log::write(int level, const char *format, ...)
 
         AppendLogLevelTitle_(level);
 
-        va_start(vaList, format);
-        int m = vsnprintf(buff_.BeginWrite(), buff_.WriteableBytes(), format, vaList);
-        va_end(vaList);
+        va_start(vaList, format);   //初始化vaList
+        int m = vsnprintf(buff_.BeginWrite(), buff_.WriteableBytes(), format, vaList);  //将vaList中的参数以format格式写入缓冲区
+        va_end(vaList); //释放vaList相关资源
 
         buff_.HasWritten(m);
         buff_.Append("\n\0", 2);
 
-        if (isAsync_ && deque_ && !deque_->full())
+        if (isAsync_ && deque_ && !deque_->full())  //异步
         {
             deque_->push_back(buff_.RetrieveAllToStr());
+            return;
         }
-        else
+        else    //同步
         {
-            fputs(buff_.Peek(), fp_);
+            fputs(buff_.Peek(), fp_);   //将缓冲区内容追加写入文件，遇到\0停止，先写到 FILE 缓冲区（用户态），不保证写到磁盘
         }
         buff_.RetrieveAll();
     }
@@ -166,16 +168,16 @@ void Log::AppendLogLevelTitle_(int level)
         buff_.Append("[debug]: ", 9);
         break;
     case 1:
-        buff_.Append("[info]: ", 9);
+        buff_.Append("[info]: ", 8);
         break;
     case 2:
-        buff_.Append("[warn]: ", 9);
+        buff_.Append("[warn]: ", 8);
         break;
     case 3:
         buff_.Append("[error]: ", 9);
         break;
     default:
-        buff_.Append("[info]: ", 9);
+        buff_.Append("[info]: ", 8);
         break;
     }
 }
@@ -186,7 +188,7 @@ void Log::flush()
     {
         deque_->flush();
     }
-    fflush(fp_);
+    fflush(fp_);    //强制把用户态缓冲区中的日志数据立即写入到文件（内核态）
 }
 
 void Log::AsyncWrite_()
